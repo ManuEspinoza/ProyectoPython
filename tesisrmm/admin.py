5fc58f1dec4086ffee47cb50d51edb6636ea6ccc
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2019-07-11 11:49:13 -0400

    Modelos agregados para la base de datos

--- tesisrmm/admin.py
+++ tesisrmm/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+from django.contrib import admin 
+ 
+from .models import Tesis, Evaluador, Autor, PalabraClave
+ 
+class EvaluadorInline(admin.StackedInline):     
+    model = Evaluador     
+    extra = 1
 
-# Register your models here.
+class AutorInline(admin.StackedInline):     
+    model = Autor     
+    extra = 1
+
+class PalabraClaveInline(admin.StackedInline):     
+    model = PalabraClave     
+    extra = 1
+
+class TesisAdmin(admin.ModelAdmin):
+    fieldsets = [('Informacion de la tesis', {'fields': ['nombre','fecha','resumen', 'calificacion','estatus','escuela']}),]
+    inlines = [PalabraClaveInline,EvaluadorInline,AutorInline] 
+ 
+admin.site.register(Tesis, TesisAdmin) 
--- tesisrmm/models.py
+++ tesisrmm/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from enum import Enum
 
-# Create your models here.
+class Status(Enum):
+    Finalizado = "Finalizado"
+    Ejecucion = "En ejecución"
+
+class Tesis(models.Model):
+    nombre = models.CharField(max_length=120)
+    fecha = models.DateTimeField('fecha de publicación')
+    resumen =  models.TextField()
+    calificacion = models.IntegerField(default=1,validators=[MinValueValidator(1), MaxValueValidator(20)])
+    estatus = models.CharField(
+      max_length=5,
+      choices=[(tag, tag.value) for tag in Status]
+    )
+    escuela = models.CharField(max_length=120)
+    
+class Autor(models.Model):
+    tesis = models.ForeignKey(Tesis, on_delete=models.CASCADE) 
+    nombre = models.CharField(max_length=120)
+    apellido = models.CharField(max_length=120)
+    correo = models.CharField(max_length=120)
+    escuela = models.CharField(max_length=120)
+
+class PalabraClave(models.Model):
+    tesis = models.ForeignKey(Tesis, on_delete=models.CASCADE) 
+    palabra = models.CharField(max_length=120)
+
+class Evaluador(models.Model):
+    tesis = models.ForeignKey(Tesis, on_delete=models.CASCADE) 
+    nombre = models.CharField(max_length=120)
+    apellido = models.CharField(max_length=120)
+    tutor = models.BooleanField(default=False)
+
+
