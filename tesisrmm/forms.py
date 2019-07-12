f103939aadd92dc13a12f69bc0d91e33ea3662a3
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2019-07-11 22:27:49 -0400

    Formulario con la fecha

--- tesisrmm/forms.py
+++ tesisrmm/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from .models import Tesis, Evaluador, Autor, PalabraClave
+from tempus_dominus.widgets import DatePicker
 
 class TesisForm(forms.ModelForm):
-
+    fecha = forms.DateField(widget=DatePicker(attrs={'autocomplete':'off'}))
     class Meta:
         model = Tesis
         fields = '__all__'
+        widgets = {
+                'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre', 'autocomplete':'off'}),
+                'resumen': forms.Textarea(attrs={'class': 'form-control', 'rows':3, 'autocomplete':'off'}),
+                'calificacion': forms.NumberInput(attrs={'class': 'form-control', 'autocomplete':'off'}),
+                'estatus': forms.Select(attrs={'class': 'form-control', 'autocomplete':'off'}),
+                'escuela': forms.TextInput(attrs={'class': 'form-control', 'autocomplete':'off'}),
+        }
+        
 
 class PalabraClaveForm(forms.ModelForm):
 
