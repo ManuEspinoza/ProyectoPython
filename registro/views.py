dad43aa452d3c2089ee91770ca0b1bd0d07fabb2
Unknown <manuel.espinoza1996@gmail.com>
2019-07-15 12:53:53 -0400

    Cambios sencillos

--- registro/views.py
+++ registro/views.py
@@ -1,24 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegistrationForm
 from django.contrib.auth.decorators import permission_required
+from tesisrmm.views import index
 
 @permission_required('auth.add_user')
 def registrar(request):
 
     if request.method == "POST":
         userFrom = RegistrationForm(request.POST)
-    
-
         if userFrom.is_valid():
             userFrom.save()
             usuario= User.objects.get(username=request.POST.get("username"))
             value = request.POST.get('rol', None)
             usuario.groups.add(value) 
-            return render(request, 'tesisrmm/inicio.html')
-              
+            return redirect(index)        
     else:
         userFrom = RegistrationForm()
         
